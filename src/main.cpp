6473e0c6b83c215a92295f89421df78670c149f6
Masonlet <masonletoile@hotmail.com>
2025-10-27 21:28:45 -0400

    Initial image loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-#include "StarletSerializer/Parser.hpp" 
+#include "StarletSerializer/parser/bmpParser.hpp" 
+#include "StarletSerializer/data/bmpData.hpp"
+#include "StarletSerializer/utils/log.hpp"
 
 int main(int argc, char* argv[]) {
+	const std::string assetPath = std::string(ASSET_DIR);
+	Starlet::Serializer::BmpParser parser;
+	Starlet::Serializer::BmpData data;
 
+	if (!parser.parse(assetPath + "/skull.bmp", data)) {
+		Starlet::Serializer::error("Image Tester", "parse", "Failed to parse image");
+		return EXIT_FAILURE;
+	}
+
+	Starlet::Serializer::debugLog("Image Tester", "main", "Image parsed successfully");
+	return EXIT_SUCCESS;
 }
+
